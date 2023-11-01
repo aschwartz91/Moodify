ba3052a47e0d10db81b80f6d383784f696b0020f
aschwartz91 <77259180+aschwartz91@users.noreply.github.com>
2023-11-01 10:19:10 -0500

    initial commit

---
+++ Moodify/Moodify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moodify
+//
+//  Created by Adam Schwartz on 11/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
