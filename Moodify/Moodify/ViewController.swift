0a01d025575b72dc2a45e2c4c3f0b22677f7ee66
aschwartz91 <77259180+aschwartz91@users.noreply.github.com>
2023-11-01 10:42:58 -0500

    landing page front end completed

--- Moodify/Moodify/ViewController.swift
+++ Moodify/Moodify/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var connectWithSpotify: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        connectWithSpotify.clipsToBounds = true
+
     }
 
-
+    @IBAction func connectWithSpotifyPressed(_ sender: Any) {
+        //Spotify credentials authorization...
+    }
+    
 }
 
