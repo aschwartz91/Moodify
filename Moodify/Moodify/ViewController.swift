26ce35dcf0c7487c000f9cb1270d70fd329f6812
trevorperez10 <37251683+trevorperez10@users.noreply.github.com>
2023-11-11 15:01:23 -0600

    spotify user authentication

--- Moodify/Moodify/ViewController.swift
+++ Moodify/Moodify/ViewController.swift
@@ -9,19 +9,162 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var connectWithSpotify: UIButton!
-    
+    // MARK: - Spotify Authorization & Configuration
+    var responseCode: String? {
+        didSet {
+            fetchAccessToken { (dictionary, error) in
+                if let error = error {
+                    print("Fetching token request error \(error)")
+                    return
+                }
+                let accessToken = dictionary!["access_token"] as! String
+                DispatchQueue.main.async {
+                    self.appRemote.connectionParameters.accessToken = accessToken
+                    self.appRemote.connect()
+                }
+            }
+        }
+    }
+
+    lazy var appRemote: SPTAppRemote = {
+        let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
+        appRemote.connectionParameters.accessToken = self.accessToken
+        appRemote.delegate = self
+        return appRemote
+    }()
+
+    var accessToken = UserDefaults.standard.string(forKey: accessTokenKey) {
+        didSet {
+            let defaults = UserDefaults.standard
+            defaults.set(accessToken, forKey: accessTokenKey)
+        }
+    }
+
+    lazy var configuration: SPTConfiguration = {
+        let configuration = SPTConfiguration(clientID: spotifyClientId, redirectURL: redirectUri)
+        configuration.playURI = ""
+        configuration.tokenSwapURL = URL(string: "http://localhost:1234/swap")
+        configuration.tokenRefreshURL = URL(string: "http://localhost:1234/refresh")
+        return configuration
+    }()
+
+    lazy var sessionManager: SPTSessionManager? = {
+        print("Session initiated successfully with access token")
+        let manager = SPTSessionManager(configuration: configuration, delegate: self)
+        return manager
+    }()
+
+    private var lastPlayerState: SPTAppRemotePlayerState?
+
+
+    // MARK: App Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        connectWithSpotify.clipsToBounds = true
 
     }
 
-    @IBAction func connectWithSpotifyPressed(_ sender: Any) {
-        //Spotify credentials authorization...
+
+    // MARK: - Actions
+    
+    @IBAction func didTapConnect(_ sender: Any) {
+        guard let sessionManager = sessionManager else { return }
+        sessionManager.initiateSession(with: scopes, options: .clientOnly)
     }
     
+
 }
 
+
+// MARK: - SPTAppRemoteDelegate
+extension ViewController: SPTAppRemoteDelegate {
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+        appRemote.playerAPI?.delegate = self
+        appRemote.playerAPI?.subscribe(toPlayerState: { (success, error) in
+            if let error = error {
+                print("Error subscribing to player state:" + error.localizedDescription)
+            }
+        })
+    }
+
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+        lastPlayerState = nil
+    }
+
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+        lastPlayerState = nil
+    }
+}
+
+// MARK: - SPTAppRemotePlayerAPIDelegate
+extension ViewController: SPTAppRemotePlayerStateDelegate {
+    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+        debugPrint("Spotify Track name: %@", playerState.track.name)
+    }
+}
+
+// MARK: - SPTSessionManagerDelegate
+extension ViewController: SPTSessionManagerDelegate {
+    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+        if error.localizedDescription == "The operation couldn’t be completed. (com.spotify.sdk.login error 1.)" {
+            print("AUTHENTICATE with WEBAPI")
+        } else {
+            print("AUTHENTICATE failed")
+        }
+    }
+
+    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
+       print("Session Renewed")
+    }
+
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        appRemote.connectionParameters.accessToken = session.accessToken
+        appRemote.connect()
+    }
+}
+
+// MARK: - Networking
+extension ViewController {
+
+    func fetchAccessToken(completion: @escaping ([String: Any]?, Error?) -> Void) {
+        let url = URL(string: "https://accounts.spotify.com/api/token")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let spotifyAuthKey = "Basic \((spotifyClientId + ":" + spotifyClientSecretKey).data(using: .utf8)!.base64EncodedString())"
+        request.allHTTPHeaderFields = ["Authorization": spotifyAuthKey,
+                                       "Content-Type": "application/x-www-form-urlencoded"]
+
+        var requestBodyComponents = URLComponents()
+        let scopeAsString = stringScopes.joined(separator: " ")
+
+        requestBodyComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: spotifyClientId),
+            URLQueryItem(name: "grant_type", value: "authorization_code"),
+            URLQueryItem(name: "code", value: responseCode!),
+            URLQueryItem(name: "redirect_uri", value: redirectUri.absoluteString),
+            URLQueryItem(name: "code_verifier", value: ""), // not currently used
+            URLQueryItem(name: "scope", value: scopeAsString),
+        ]
+
+        request.httpBody = requestBodyComponents.query?.data(using: .utf8)
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data,                              // is there data
+                  let response = response as? HTTPURLResponse,  // is there HTTP response
+                  (200 ..< 300) ~= response.statusCode,         // is statusCode 2XX
+                  error == nil else {                           // was there no error, otherwise ...
+                      print("Error fetching token \(error?.localizedDescription ?? "")")
+                      return completion(nil, error)
+                  }
+            let responseObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any]
+            if let accessToken = responseObject?["access_token"] as? String {
+                        print("Successfully fetched access token: \(accessToken)")
+                    } else {
+                        print("Failed to fetch access token.")
+                    }
+            print("Access Token Dictionary=", responseObject ?? "")
+            completion(responseObject, nil)
+        }
+        task.resume()
+    }
+
+}
